b649d99d5c982e45c92265acd780e07d2c1acae7
MihaiDiaconu12 <79709451+MihaiDiaconu12@users.noreply.github.com>
2022-05-24 17:06:01 +0300

    Add files via upload

--- FoodPandaAssignment2/spersamergi/src/main/java/com/example/spersamergi/restaurantPackage/RestaurantController.java
+++ FoodPandaAssignment2/spersamergi/src/main/java/com/example/spersamergi/restaurantPackage/RestaurantController.java
@@ -24,6 +24,12 @@ public class RestaurantController {
     @Autowired
     RestaurantService restaurantService;
 
+    /**
+     * This is the method that adds a restaurant to the database. It searches in the database if the restaurant is
+     * already there and if not, the new restaurant will be added.
+     * @param restaurantDTO it is the structure that contains the strings given as input for the restaurant
+     * @return A string if the restaurant is already in the database and a restaurant if it has been successfully added
+     */
     @PostMapping("/addRestaurant")
     public ResponseEntity<?> enterRestaurant(@RequestBody restaurantDTO restaurantDTO){
         Restaurant newRestaurant=new Restaurant(restaurantDTO.restaurantName,restaurantDTO.restaurantPassword,
@@ -46,6 +52,10 @@ public class RestaurantController {
         return new ResponseEntity<Restaurant>(newRestaurant,HttpStatus.CREATED);
     }
 
+    /**
+     * It is exactly what it says. It gets a list of all the restaurants in the database and returns it.
+     * @return it returns a list of all the restaurants. Used both by the administrator and the client.
+     */
     @PostMapping("/getAllRestaurants")
     public ResponseEntity<List<Restaurant>> getAllRestaurants(){
         List<Restaurant> restaurants;
