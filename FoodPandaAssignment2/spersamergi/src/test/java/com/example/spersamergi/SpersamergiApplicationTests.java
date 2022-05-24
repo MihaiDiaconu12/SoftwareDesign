3100588fc3b322d365eb85364c0f895d22370906
MihaiDiaconu12 <79709451+MihaiDiaconu12@users.noreply.github.com>
2022-05-24 17:07:51 +0300

    Add files via upload

--- FoodPandaAssignment2/spersamergi/src/test/java/com/example/spersamergi/SpersamergiApplicationTests.java
+++ FoodPandaAssignment2/spersamergi/src/test/java/com/example/spersamergi/SpersamergiApplicationTests.java
@@ -1,13 +1,36 @@
 package com.example.spersamergi;
 
+import com.example.spersamergi.foodPackage.Food;
+import com.example.spersamergi.foodPackage.FoodService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
 class SpersamergiApplicationTests {
 
+	FoodsTest underTest = new FoodsTest();
 	@Test
-	void contextLoads() {
+	void itShouldAddFood() {
+		//given
+		Food food1=new Food("English breakfast","The best there is", (float) 2.76,"breakfast");
+		Food result = underTest.add(food1);
+		assertThat(result.getName()).isEqualTo("English breakfast");
+
+		Food food2=new Food("Scottish breakfast","The best there is", (float) 2.76,"breakfast");
+		Food result2 = underTest.add(food2);
+		assertThat(result2.getName()).isEqualTo("Scottish breakfast");
+
+		Food food3=new Food("Irish breakfast","The best there is", (float) 2.76,"breakfast");
+		Food result3 = underTest.add(food3);
+		assertThat(result3.getName()).isEqualTo("Irish breakfast");
 	}
 
+	@Autowired
+	FoodService foodService;
+	class FoodsTest{
+		Food add(Food food){return foodService.addFood(food);};
+	}
 }
